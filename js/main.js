740082e2bf382a8201373bc20fe6bd153783a31d
johanbert <johanbertmosquera@gmail.com>
2021-09-29 20:10:32 -0500

    avances de html+js en clases antes del receso

--- js/main.js
+++ js/main.js
@@ -9,8 +9,8 @@ function getPropertyExample(formName, fieldName, property) {
     return document[formName][fieldName][property]
 }
 // EJEMPLOS DE EJECUCION
-console.log("testing", getPropertyExample("formExample", "campovalor", "value"));
-console.log("testing 2", getPropertyExample("formExample2", "campovalor", "value"));
+// console.log("testing", getPropertyExample("formExample", "campovalor", "value"));
+// console.log("testing 2", getPropertyExample("formExample2", "campovalor", "value"));
 // funcion de flecha VERSION 1
 const getValue2 = () => {
     return Number(document.formExample.campo_valor.value)
@@ -34,7 +34,7 @@ var anonima = function() {
 };
 
 // Ejemplo de acceder a el array de getElementsByName
-document.getElementsByName("campovalor")[0].addEventListener("click", callbackEvent)
+// document.getElementsByName("campovalor")[0].addEventListener("click", callbackEvent)
 
 // // CALLBACK HELL
 // const funcionCallback = (argumento, callback) => {
@@ -68,8 +68,19 @@ const getProperty = (formName, fieldName, property) => document[formName][fieldN
 const arimeticOperation = (formName, tagName, firstValue, secondValue, operator) => {
     document[formName][tagName].value = eval(`${firstValue} ${operator} ${secondValue}`)
 }
+const calculate = (e, obj) => console.log(eval(`${obj.val1} ${e.target.textContent} ${obj.val2}`))
 
-const firstValue = getProperty("formExample", "campovalor", "value");
-const secondValue = getProperty("formExample", "campovalor", "value");
 
-arimeticOperation("formExample", "resultado1", firstValue, secondValue, "+")
+// document.querySelector('form div').addEventListener('click', calculate)
+const property = "value"
+let form = "formExample"
+let tagNameResult = "resultValue"
+let operator = "+"
+let jsonForm = {
+    firstVal: getProperty(form, "firstValue", property),
+    secondVal: getProperty(form, "secondValue", property)
+}
+
+// let response = arimeticOperation(form, tagNameResult, jsonForm.firstVal, jsonForm.secondVal, operator);
+
+// console.log(response);
