124d7cad4a8b8645d53dbbdb41651fc97e05a368
johanbert <johanbertmosquera@gmail.com>
2021-10-01 22:24:09 -0500

    calculadora 2 finalizada

--- js/calculadora2.js
+++ js/calculadora2.js
@@ -1,6 +1,3 @@
-// document.querySelectorAll('form[name="formExample"] div#buttons button').addEventListener('click', () => { console.log('test') })
-document.getElementById('buttons').addEventListener('click', (e) => { console.log('test', e) })
-
 // RETORNAN UNA LISTA = ARRAY = ARREGLO = MATRIZ
 // document.querySelectorAll()
 // document.getElementsByClassName()
@@ -8,4 +5,33 @@ document.getElementById('buttons').addEventListener('click', (e) => { console.lo
 // document.getElementsByTagName()
 // document.getElementsByTagNameNS()
 
-// console.log("test", );
+// if (elemento != type) { // (element.target.type != 'button')
+//     return false
+// } else {
+//     return true
+// }
+// const isValid = (elemento, type) => {
+//     return (elemento != type) ? false : true;
+// }
+
+// if (!field1.value || !field2.value) {
+//     result.value = 'hubo un error 2'
+//     return
+// }
+
+const field1 = document.getElementsByName('firstValue')[0]
+const field2 = document.getElementsByName('secondValue')[0]
+const result = document.getElementsByName('resultValue')[0]
+
+const isValid = (elemento, type) => (elemento != type) ? false : true
+const isFieldsValids = (val1, val2) => (!val1 || !val2) ? false : true
+
+const calcular = (element) => {
+    if (!isValid(element.target.type, 'button')) return
+    if (!isFieldsValids(field1.value, field2.value)) return
+
+    const operator = element.target.textContent
+    result.value = eval(` ${field1.value} ${operator} ${field2.value}`)
+}
+
+document.getElementById('buttons').addEventListener('click', calcular)
